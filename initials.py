cdcde850a4b37c92edb7b84c44c149f324e3124a
Addeyishim <2425401658@live.gctu.edu.gh>
2026-05-31 17:14:13 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials as large block letters.
+# Assignment Number: 1
+#
+# Name: Selasi Korku Addey
+# STUDENT ID:  2425401658
+# Email: 2425401658@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Selasi Korku Addey, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Print my initials (SKA) in large block letters
 
+    print()
+    print("...SKA")
+    print()
+
+    print("....SSSSSSSSSS....KK.......KK....AAAAAA....")
+    print("...SSSSSSSSSSSS...KK......KK....AAAAAAAA...")
+    print("...SSS............KK....KK......AA....AA...")
+    print("...SS.............KK..KKK.......AA....AA...")
+    print("....SSSSSSSSSS....KKKKK.........AAAAAAAAAA..")
+    print("...SSSSSSSSSSSS...KKKKK.........AAAAAAAAAA..")
+    print("............SS....KK..KKK.......AA......AA..")
+    print("...........SSS....KK....KK......AA......AA..")
+    print("...SSSSSSSSSSSS...KK......KK....AA......AA..")
+    print("....SSSSSSSSSS....KK.......KK...AA......AA..")
+
+    print()
 
 
 main()
